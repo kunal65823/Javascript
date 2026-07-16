430c1369dc6893e471b4464e29281739fbf7bbb7
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-16 17:36:59 +0000

    completed datetime

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+const numbers = new Number(1000000);
+
+console.log(numbers.toLocaleString());
+console.log(Math.min(4,5,7,8,6,3,2,1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *(max -min +1)) + min); 
