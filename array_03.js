fdb61a2f84380e1ba0c04482c284b7e9e0d3f1b4
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-16 18:54:56 +0000

    learn array

---
+++ array_03.js
@@ -0,0 +1,29 @@
+const newArr = [12,54,5,6,3,8];
+
+// console.log(newArr);
+// console.log(`Our array is ${newArr}`);
+
+// console.log(newArr.unshift(8));
+
+// newArr.unshift(5);
+// newArr.shift();
+
+// console.log(newArr);
+
+// const marvel_heros = ["Ironman", "Captain america","antman"];
+// const dc_heros= ["superman","falsh","batman"];
+
+// console.log(marvel_heros);
+// console.log(dc_heros);
+
+// const all_heros = [...dc_heros ,...marvel_heros];
+// console.log(all_heros);
+
+// console.log(Array.from("XYASD"));
+
+let a = 100;
+let b = 20;
+let c = 40000;
+
+console.log(Array.of(a,b,c));
+
