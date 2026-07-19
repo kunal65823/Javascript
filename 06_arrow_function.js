0eadead3cf2f902a955e9b773a491efbcf6f383f
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-19 13:47:43 +0000

    learn arrow functions

---
+++ 06_arrow_function.js
@@ -0,0 +1,32 @@
+// // arrow function
+
+// const user = {
+//     username: " KUnal",
+//     price:"199",
+
+
+//     welcomemessage: function(){
+//         console.log(`${this.username}, welcome to website`);
+//     }
+// }
+// user.username ="Sam";
+// user.welcomemessage();
+
+// //this cant work in function also in arror function = () =>
+
+// function chai(){
+//     let username = "kunal"
+//     console.log(this.username);
+// }
+
+// chai();
+
+// arrow function 
+
+// const addtwo = (nums1,nums2) =>{
+//     return nums1+ nums2;
+// }
+
+const addtwo = (nums1,nums2) => nums1+ nums2;
+
+console.log(addtwo(3,5));
