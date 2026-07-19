3da6285c9d7f75472f0b7c535f326c1ed1f7f2e6
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-19 15:16:30 +0000

    learn about IIFE

---
+++ 07_iife.js
@@ -0,0 +1,13 @@
+// immediately invoked function Expression (IIFE)
+
+// (function chai(){
+//     console.log("DB Connected");
+// })();
+
+// (function aurchai(){
+//     console.log("DB Connected");
+// })();
+
+(function aurchai(name){
+    console.log(`${name} Hello`);
+})("kunal");
