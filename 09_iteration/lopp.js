a99e9a73ddf4d51aeafab00593c42b3b05c73eee
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-22 16:47:20 +0000

    learn some map

--- 09_iteration/lopp.js
+++ 09_iteration/lopp.js
@@ -38,10 +38,123 @@
 
 // while
 
-let index = 0;
+// let index = 0;
 
-while (index <= 10) {
-    console.log(`value is ${index}`);
+// while (index <= 10) {
+//     console.log(`value is ${index}`);
 
-    index = index+2;
-}
+//     index = index+2;
+// }
+
+// for of 
+
+
+// const arr = [1,2,4,5,6,3,7];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello World";
+
+// for (const gret of greeting) {
+//     console.log(`each element of greeting ${gret}`);
+    
+// }
+
+// const map = new Map();
+
+// map.set('IN',"INDIA");
+// map.set('USA',"United States America");
+// map.set('Fr',"FRANCE");
+
+// for (const [key,value] of map) {
+//     console.log(key,value);
+// }
+
+// for of loop cant work on the objects
+
+// we use for in loop for objects
+
+// const myObjects = {
+//     js : "Javascript",
+//     cpp: "cpp",
+//     py: "pyhton"
+// }
+
+
+// for (const key in myObjects) {
+//     console.log(key,myObjects[key]);
+    
+// }
+
+// for each
+
+// const codiing = ["java","python","cpp","c#"];
+
+// codiing.forEach(function (val,index,arr) {
+//     console.log(val,index,arr);
+// })
+
+
+// loop for multiople objects we use foreach loop
+
+// const myObject = [
+//     {
+//         languagename:"Javascipt",
+//         languageflenme:"js"
+//     },
+//     {
+//         languagename:"cpp",
+//         languageflenme:"cpp"
+//     },
+//     {
+//         languagename:"python",
+//         languageflenme:"py"
+//     },
+//     {
+//         languagename:"java",
+//         languageflenme:"java"
+//     }
+// ]
+
+// myObject.forEach((items) => {
+//     console.log(items.languageflenme);
+// })
+
+
+// Maps, filters and other
+
+// const mynums = [1,2,3,4,5,6,7,8];
+
+// const newnums = mynums.filter((nums)=>{
+//     return nums >4;
+// })
+
+// console.log(newnums);
+
+// const books = [
+//     {
+//         title: "Atomic Habits",
+//         author: "James Clear",
+//         price: 499
+//     },
+//     {
+//         title: "Rich Dad Poor Dad",
+//         author: "Robert Kiyosaki",
+//         price: 399
+//     },
+//     {
+//         title: "The Alchemist",
+//         author: "Paulo Coelho",
+//         price: 299
+//     }
+// ];
+
+
+// let book = books.filter((bk) => bk.author == "James Clear");
+
+// book = books.filter((bk) => bk.price > 300 && bk.title ==="Rich Dad Poor Dad");
+
+// console.log(book);
+
