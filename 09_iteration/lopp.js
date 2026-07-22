87761451a07e7bcb9694d2e6644e381807e90f3a
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-22 01:49:25 +0000

    learn some loop

---
+++ 09_iteration/lopp.js
@@ -0,0 +1,47 @@
+//for 
+// for (let index = 0; index < 10;index++) {
+//     const element =index;
+//     console.log(element); 
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     console.log("outer elemt");
+//     for (let j = 0; j < 10; j++) {
+    
+//         console.log("inner elemnt");
+        
+//     }
+    
+// }
+
+//  for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         console.log(   `${i} + ${j} = ${i*j}`);
+//     }
+
+//     let myArray = ["Ironman","Batman", "Thor"];
+// for (let index = 0; index < myArray.length; index++) {
+    
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+// for (let index = 0; index < 10; index++) {
+//     if(index == 5){
+//         console.log("Deted NO 5");
+//         break;
+//     }
+//         console.log(`value of i is ${index}`);
+    
+// }
+
+// while
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value is ${index}`);
+
+    index = index+2;
+}
