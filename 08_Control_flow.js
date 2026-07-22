b1169dce01934ccbaefd2bff3b6a216bca771309
Kunal Maruti Shitole <kunal.shitole21@gmail.com>
2026-07-22 01:10:24 +0000

    learn control flow

---
+++ 08_Control_flow.js
@@ -0,0 +1,48 @@
+//
+
+// const score = 200;3
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`our power is ${power}`);
+
+//     const UserloggefIn = true;
+//     const debitcard = true;
+
+//     if(UserloggefIn && debitcard){
+//         console.log("Allw to user shopping");
+//     }
+
+// const month = "March";
+
+// switch (month) {
+//     case "jan":
+//         console.log("Jan");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "March":
+//         console.log("march");
+//         break;
+
+//     default:
+//         console.log("Nothing mactvh");
+//         break;
+// }
+
+
+// nullish coeleshng Operator ??
+
+let val1;
+// val1 = 5?? 11;
+val1 = null ?? 10;
+
+console.log(val1);
+
+
+// ternary operator 
+
+// consition ?? true : false;
+
+
